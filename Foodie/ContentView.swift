4b36b6e1c666822a27e6349b16ede0af300b8a87
ashutoshpandey28 <ashutoshpandey.developer@gmail.com>
2021-07-04 19:32:00 +0530

    Initial Commit

---
+++ Foodie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foodie
+//
+//  Created by ashutosh on 04/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by ashutosh on 04/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
